754ac156a4c47a183f79ffaa117d82490512cb44
Calvin445 <cai.ca@husky.neu.edu>
2019-09-21 16:40:03 -0400

    update education

--- src/education.js
+++ src/education.js
@@ -6,20 +6,19 @@ const edu = new Vue({
         <p class="rB">
         <strong>Northeastern University</strong>, Boston MA </br> Candidate for a BS in Computer Science and Mathematics
         <div class="table">
-            <p class="rB tableHead">Courses:</p>
+            <p class="rB tableHead">Courses I really enjoyed:</p>
             <p class="rB tableContents">
                 Object Oriented Design | Algorithms and Data | Computer Systems |
-                Data Collection, Integration and Analysis | Discrete Structures |
-                Logic and Computation | Discrete Structures | Linear Algebra | Calculus 3
+                Data Collection, Integration and Analysis | Discrete Structures | Linear Algebra | Calculus 3
             </p>
         </div>
         <div class="table">
             <p class="rB tableHead">GPA:</p>
-            <p class="rB tableContents">3.94 / 4.00</p>
+            <p class="rB tableContents">3.95 / 4.00</p>
         </div>
         <div class="table">
             <p class="rB tableHead">Honors:</p>
-            <p class="rB tableContents">Dean's List, Honors Program</p>
+            <p class="rB tableContents">Dean's List, Honors Program, Khoury College Fellow</p>
         </div>    
     </div>`
 })
