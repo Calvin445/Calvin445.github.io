1422f99b4d76cefea5f2f585ca196a4181f21a7d
Calvin445 <cai.ca@husky.neu.edu>
2019-09-21 15:05:24 -0400

    convert to vue

---
+++ src/education.js
@@ -0,0 +1,25 @@
+const edu = new Vue({
+    el: '#education',
+    template: `    
+        <div class="section" id="coursework">
+        <h3 class="rR">EDUCATION</h3>
+        <p class="rB">
+        <strong>Northeastern University</strong>, Boston MA </br> Candidate for a BS in Computer Science and Mathematics
+        <div class="table">
+            <p class="rB tableHead">Courses:</p>
+            <p class="rB tableContents">
+                Object Oriented Design | Algorithms and Data | Computer Systems |
+                Data Collection, Integration and Analysis | Discrete Structures |
+                Logic and Computation | Discrete Structures | Linear Algebra | Calculus 3
+            </p>
+        </div>
+        <div class="table">
+            <p class="rB tableHead">GPA:</p>
+            <p class="rB tableContents">3.94 / 4.00</p>
+        </div>
+        <div class="table">
+            <p class="rB tableHead">Honors:</p>
+            <p class="rB tableContents">Dean's List, Honors Program</p>
+        </div>    
+    </div>`
+})
